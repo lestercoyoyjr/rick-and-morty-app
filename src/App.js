0932ebfb3d81d699a4921c4db98803f9f40995dd
lestercoyoy <lester_cch@hotmail.com>
2022-11-07 19:51:50 -0600

    ADD
    API rick and morty URL consumption

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import Navbar from './Components/Navbar'
 
+fetch("https://rickandmortyapi.com/api/character")
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error))
 
 function App() {
   return (
