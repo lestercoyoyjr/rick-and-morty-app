b0af88336c0c46346efd3a54f15f5d9c0243bfcd
lestercoyoy <lester_cch@hotmail.com>
2022-11-07 19:33:56 -0600

    ADD
    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import Navbar from './Components/Navbar'
+
 
 function App() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand text-uppercase" href="/">Rick & Morty App</a>
-      </div>
-    </nav>
+    <Navbar brand="Rick and Morty App"/>
   );
 }
 
