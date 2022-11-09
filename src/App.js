5bdddef705c1088f046dd0556b1a872bb749f0ed
lestercoyoy <lester_cch@hotmail.com>
2022-11-09 11:03:46 -0600

    ADD
    Characters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Components/Navbar'
 
 function App() {
 
+  const [characters, setCharacters] = useState([]);
+
   const initialUrl = "https://rickandmortyapi.com/api/character";
 
-  const fetchCharacters = (initialUrl) => {
-    fetch(initialUrl)
+  const fetchCharacters = (url) => {
+    fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => setCharacters(data.results))
       .catch(error => console.log(error))
   };
 
