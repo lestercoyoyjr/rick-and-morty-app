830d0712c5c4d2aa0a4a7a254954a0746092a4a3
lestercoyoy <lester_cch@hotmail.com>
2022-11-09 11:11:28 -0600

    ADD
    HTML components

--- src/App.js
+++ src/App.js
@@ -19,7 +19,53 @@ function App() {
   }, [])
 
   return (
-    <Navbar brand="Rick and Morty App" />
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container py-5">
+        <nav>
+          <ul className="pagination justify-content-center">
+            {info.prev ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handlePreviousPage}>
+                  Previous
+                </button>
+              </li>
+            ) : null}
+            {info.next ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handleNextPage}>
+                  Next
+                </button>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
+
+      <CharacterList characters={characters} />
+
+      <div className="container pb-5">
+        <nav>
+          <ul className="pagination justify-content-center">
+            {info.prev ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handlePreviousPage}>
+                  Previous
+                </button>
+              </li>
+            ) : null}
+            {info.next ? (
+              <li className="page-item">
+                <button className="page-link" onClick={handleNextPage}>
+                  Next
+                </button>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
